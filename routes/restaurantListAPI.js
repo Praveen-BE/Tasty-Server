a6ab87bd7a6c10a3151852060c2a48ba826d2b22
Praveen-BE <praveenrajendiran3939@gmail.com>
2025-01-27 23:46:48 +0530

    api currection

--- routes/restaurantListAPI.js
+++ routes/restaurantListAPI.js
@@ -6,7 +6,7 @@ const restaurantListRouter = express.Router();
 restaurantListRouter.get("/restuarantList/:page", async (req, res) => {
   try {
     const paramValue = req.params.page;
-    const PageNo = parseInt(paramValue) || 0;
+    const PageNo = parseInt(paramValue) - 1 || 0;
     res.json(bangalore[PageNo]);
   } catch (error) {
     res.json({ data: { message: error.message } });
